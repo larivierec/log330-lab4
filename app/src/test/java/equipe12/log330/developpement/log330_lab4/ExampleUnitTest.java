c39e3568e88dd112be96ce71379d9c2a9776ba23
Christopher Larivière <lariviere.c@gmail.com>
2015-10-07 17:07:22 -0400

    initial commit

---
+++ app/src/test/java/equipe12/log330/developpement/log330_lab4/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package equipe12.log330.developpement.log330_lab4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
