085c0551d7a2a13c167c05c3a6da03e2a14d23ab
Christopher Larivière <lariviere.c@gmail.com>
2015-10-17 17:51:56 -0400

    Merge branch 'master' of M:\gitStuff\log330-lab4 with conflicts.

--- app/src/main/java/equipe12/log330/developpement/log330_lab4/interfaces/DialogGPSAcceptedListener.java
+++ app/src/main/java/equipe12/log330/developpement/log330_lab4/interfaces/DialogGPSAcceptedListener.java
@@ -3,6 +3,6 @@ package equipe12.log330.developpement.log330_lab4.interfaces;
 /**
  * Created by Chris on 10/9/2015.
  */
-public interface DialogGPSAccepted {
+public interface DialogGPSAcceptedListener {
     void onDialogButtonAdded(String gpsName, String gpsID, String gpsType);
 }
