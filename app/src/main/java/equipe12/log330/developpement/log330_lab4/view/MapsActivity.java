bd7c68129308167a8d00a37e8abe73b803b1ee39
Christopher Larivière <lariviere.c@gmail.com>
2015-10-26 15:45:48 -0400

    GPS Menu Added gps menu and save option. TODO: do the implementation logic

--- app/src/main/java/equipe12/log330/developpement/log330_lab4/view/MapsActivity.java
+++ app/src/main/java/equipe12/log330/developpement/log330_lab4/view/MapsActivity.java
@@ -3,6 +3,8 @@ package equipe12.log330.developpement.log330_lab4.view;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -95,5 +97,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .radius(50000)
                     .strokeColor(Color.BLUE));
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.gps_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.gps_save_zone) {
+            //TODO save the zone to the database
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
